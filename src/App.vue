3d3fc078ae4ba4f2b687a4150f39efde6bf7a0f6
bonji810 <yunajimbou810@gmail.com>
2022-02-20 18:50:51 +0900

    feat: 🎸 create project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import EasingEditor from './components/EasingEditor.vue'
+</script>
+
+<template>
+ <easing-editor />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
